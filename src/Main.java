8e57dc98e7f12f4aac9aee332406e9b39f1eb093
almunimax <almunimax@mail.ru>
2022-12-08 20:40:41 +0300

    выполнено задача 1-4

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,25 @@ public class Main {
         // задание 1
         System.out.println("задание 1");
 
-        for ( int i = 0; i < 10; i++){
-            System.out.println("Итерация цикла" + i);
+        for ( int i = 1; i <= 10; i++) {
+            System.out.println("Итерация цикла " + i);
+        }
+            // задание 2
+            System.out.println("задание 2");
 
+            for ( int z = 10; z >= 1; z--) {
+                System.out.println("Итерация цикла " + z);
+            }
+            // задание 3
+                 System.out.println("задание 3");
 
+            for ( int x = 0; x < 17; x = x + 2) {
+                System.out.println("Итерация цикла " + x);
+            }
+            // задание 4
+                System.out.println("задание 4");
+
+            for ( int с = 10; с >= -10; с = с - 1) {
+                System.out.println("Итерация цикла " + с);}
         }
     }
-}
