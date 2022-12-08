eb11e89e74e4391f755d8de96b7867fa9f78459b
almunimax <almunimax@mail.ru>
2022-12-08 19:36:57 +0300

    выполнено

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        // задание 1
+        System.out.println("задание 1");
+
+        for ( int i = 0; i < 10; i++){
+            System.out.println("Итерация цикла" + i);
+
+
+        }
+    }
+}
